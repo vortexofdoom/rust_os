67ae911850c577cda569d86bb00c20e6699b6d2b
Daniel Larson <vortexofdoom97@gmail.com>
2023-02-04 16:52:12 -0600

    testing framework

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,28 +9,17 @@ use rust_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{info}");
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
