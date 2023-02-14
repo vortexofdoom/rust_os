1dad8c9ae9a3a90c43626047e363d08ae021b339
Daniel Larson <vortexofdoom97@gmail.com>
2023-02-14 16:23:31 -0600

    Multitasking basics

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,16 @@
 
 extern crate alloc;
 
-use alloc::{
-    boxed::Box,
-    vec,
-    vec::Vec,
-    rc::Rc,
-};
 use core::panic::PanicInfo;
-use rust_os::{println, memory::BootInfoFrameAllocator};
+use rust_os::{
+    println, 
+    memory::BootInfoFrameAllocator,
+    task::{
+        keyboard,
+        Task, 
+        executor::Executor,
+    },
+};
 use bootloader::{BootInfo, entry_point};
 
 #[alloc_error_handler]
@@ -38,29 +40,24 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     allocator::init_heap(&mut mapper, &mut frame_allocator)
         .expect("heap initialization failed");
     
-    // allocate on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    let ref_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_ref = ref_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_ref));  
-    core::mem::drop(ref_counted);
-    println!("reference count is now {}", Rc::strong_count(&cloned_ref));  
-
-
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
-    rust_os::hlt_loop();
+
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {number}");
 }
 
 #[cfg(not(test))]
